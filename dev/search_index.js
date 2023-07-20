a37df4dd7a4d99aa36834dd093cf459c6b31b80b
Documenter.jl <documenter@juliadocs.github.io>
2023-07-20 03:42:00 +0000

    build based on 83172f7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = julia_repo_setup","category":"page"},{"location":"#julia*repo*setup","page":"Home","title":"juliareposetup","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for juliareposetup.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [julia_repo_setup]","category":"page"}]
+}
